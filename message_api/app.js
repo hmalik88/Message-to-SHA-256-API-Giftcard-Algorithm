b78e93965296b3afe7206e67fa23391023e6fbbf
hmalik88 <hbmalik88@gmail.com>
2019-10-29 06:16:25 -0400

    update error handling

--- message_api/app.js
+++ message_api/app.js
@@ -9,10 +9,4 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use('/messages', messageRoutes);
 
-app.use((req, res, next) => {
-  const error = new Error('Message not found');
-  error.status = 404;
-  next(error);
-})
-
 module.exports = app;
--- message_api/routes/messages.js
+++ message_api/routes/messages.js
@@ -14,9 +14,15 @@ router.post('/', (req, res, next) => {
 router.get('/:hash', (req, res, next) => {
   const hash = req.params.hash;
   const message = hashes[hash];
-  res.status(200).json({
+  if (!message) {
+    res.status(404).json({
+      err_msg: 'Message not found'
+    })
+  } else {
+    res.status(200).json({
     message: message
   })
+  }
 })
 
 
